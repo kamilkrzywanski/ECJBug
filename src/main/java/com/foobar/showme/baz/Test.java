88b40620092e575d00c094ddad076653aa49a5c2
kkrzywanski <k.krzywanski@aasys.pl>
2025-03-13 15:21:41 +0100

    Bug commit

--- src/main/java/com/foobar/showme/baz/Test.java
+++ src/main/java/com/foobar/showme/baz/Test.java
@@ -6,16 +6,10 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class Test {
     public static Pair<Long, Long> numericRangeFromString(String range) {
-        if (StringUtils.isNotEmpty(range)) {
-            String[] rangeStr = range.split("-");
-            try {
-                return new ImmutablePair<>(
-                        rangeStr[0] != null ? Long.parseLong(rangeStr[0]) : null,
-                        rangeStr.length > 1 && rangeStr[1] != null ? Long.parseLong(rangeStr[1]) : null);
-            } catch (Exception e) {
-                return null;
-            }
-        }
-        return null;
+        String[] rangeStr = range.split("-");
+        return new ImmutablePair<>(
+                rangeStr[0] != null ? Long.parseLong(rangeStr[0]) : null,
+                rangeStr.length > 1 && rangeStr[1] != null ? Long.parseLong(rangeStr[1]) : null
+        );
     }
 }
