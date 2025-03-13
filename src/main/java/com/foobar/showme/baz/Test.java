75db4c789d379bfb31603975015ebfec67bc71e6
Kamil Krzywanski <kamilkrzywanski01@gmail.com>
2025-03-13 21:03:11 +0100

    #also with map

--- src/main/java/com/foobar/showme/baz/Test.java
+++ src/main/java/com/foobar/showme/baz/Test.java
@@ -4,10 +4,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Map;
+
 public class Test {
-    public static Pair<Long, Long> numericRangeFromString(String range) {
+    public static Map<Long, Long> numericRangeFromString(String range) {
         String[] rangeStr = range.split("-");
-        return new ImmutablePair<>(
+        return Map.of(
                 rangeStr[0] != null ? Long.parseLong(rangeStr[0]) : null,
                 rangeStr.length > 1 && rangeStr[1] != null ? Long.parseLong(rangeStr[1]) : null
         );
